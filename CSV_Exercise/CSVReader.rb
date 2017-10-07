daff4dc0fd8cf5c6ceb90d13d9e9c21fb157793b
bhoomika08 <bhoomika@vinsol.com>
2017-10-07 12:15:38 +0530

    CSV_Exercise

--- CSV_Exercise/CSVReader.rb
+++ CSV_Exercise/CSVReader.rb
@@ -1,5 +1,6 @@
 require 'csv'
 
+# Class to display designation, name and id of employee
 class Employee
   attr_reader :name, :emp_id, :designation
 
@@ -9,26 +10,29 @@ class Employee
   end
 
   def to_s
-    "#{ @name } (Employee Id: #{ @emp_id })"
-  end 
+    "#{@name} (Employee Id: #{@emp_id})"
+  end
 end
 
-class CSVReader < Employee
+# Class to read the csv and write to other file.
+class CSVReadWrite
   def initialize
     @employees = Hash.new { |hash, key| hash[key] = [] }
   end
 
   def csv_read
-    CSV.foreach('Employee.csv', headers: true) do |row|
+    csvfile = File.join(File.dirname(__FILE__), 'Employee.csv')
+    CSV.foreach(csvfile, headers: true) do |row|
       @employees[row['Designation'].to_sym] << Employee.new(row['Name'], row['EmpId'])
-    end 
+    end
   end
-  
-  def write_to(file_name)     
+
+  def write_to(file_name)
     File.open(file_name, 'w') do |file|
-      @employees.each do |designation, value|   
+      @employees.sort.each do |designation, value|
         @employees[designation].length <= 1 ? (file.puts "#{designation}") : (file.puts "#{designation}s")
-        file.puts value
+        values = value.sort_by { |emp| emp.emp_id }
+        file.puts values
         file.puts
       end
     end
@@ -37,7 +41,10 @@ end
 
 puts 'Enter file name to be created'
 filename = gets.chomp
-csv_reader = CSVReader.new
+csv_reader = CSVReadWrite.new
 csv_reader.csv_read
-csv_reader.write_to(filename)
- 
+if filename.empty?
+  csv_reader.write_to('output.txt')
+else
+  csv_reader.write_to(filename)
+end
